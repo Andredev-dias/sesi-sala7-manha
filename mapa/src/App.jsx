01babd847e22b0eea28521969c22ae5bec0e0d17
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-05-09 12:03:43 -0300

    opa

--- mapa/src/App.jsx
+++ mapa/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const position = [-25.424847, -49.2749795]
   return (
     <>
     <h1>Turma 7</h1>
+    <br />
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: "100%", height: "800px"}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </>
   )
 }
